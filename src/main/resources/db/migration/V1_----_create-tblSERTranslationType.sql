d60e76e8de6a94aca79fc9e1eb8f5ee3850fe54e
Pavelzzzzz <pavel@localhost.localdomain>
2018-02-21 10:48:02 +0300

    +SQL version 1

--- src/main/resources/db/migration/V1_----_create-tblSERTranslationType.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS tblSERTranslationType (
-  `TranslationTypeId` INT NOT NULL AUTO_INCREMENT,
-  `TranslationTypeCode` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`TranslationTypeId`),
-  UNIQUE INDEX `TranslationTypeCode_UNIQUE` (`TranslationTypeCode` ASC));
